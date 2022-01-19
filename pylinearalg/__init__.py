b25bf4b9c7d9d7e43bf7d87a96b0a5255b696c4a
Thomas Breydo <tbreydo@gmail.com>
2022-01-19 12:33:19 -0500

    Bump version

--- pylinearalg/__init__.py
+++ pylinearalg/__init__.py
@@ -3,3 +3,6 @@ from .vector import BasisVector
 from .vector import Constant
 from .vector import Vector
 from .vector import VectorSpace
+
+__version__ = "0.2.0"
+__all__ = ['gram_schmidt', 'BasisVector', 'Constant', 'Vector', 'VectorSpace']
